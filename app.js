ea80a35fdd306e4ce633f40f9f110762324c3052
gautamjuyal <gautam.juyal.94@gmail.com>
2023-05-15 00:04:53 +0530

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const notesController = require("./controllers/notesController");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const getAllNotes = (req, res) => {};
 
